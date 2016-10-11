e8e1bcfd06ae64d21fea719c0d833728ebfe6c42
Ojas Gosar <ojas.gosar@gmail.com>
2016-10-11 11:19:52 -0400

    making use of beepboop-botkit for slack app support for multi-channel support

--- index.js
+++ index.js
@@ -44,11 +44,12 @@
 var Botkit = require('botkit');
 var Https = require('https');
 var Moment = require('moment-timezone');
+var BeepBoop = require('beepboop-botkit');
 
-if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFY_TOKEN) {
-    console.log('Error: Specified CLIENT_ID:'+ process.env.CLIENT_ID +', CLIENT_SECRET:' + process.env.CLIENT_SECRET + ', VERIFY_TOKEN:' + process.env.VERIFY_TOKEN + ', PORT:' + process.env.PORT + 'in environment');
-    process.exit(1);
-}
+// if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT || !process.env.VERIFY_TOKEN) {
+//     console.log('Error: Specified CLIENT_ID:'+ process.env.CLIENT_ID +', CLIENT_SECRET:' + process.env.CLIENT_SECRET + ', VERIFY_TOKEN:' + process.env.VERIFY_TOKEN + ', PORT:' + process.env.PORT + 'in environment');
+//     process.exit(1);
+// }
 
 var config = {}
 if (process.env.MONGOLAB_URI) {
@@ -64,93 +65,99 @@ if (process.env.MONGOLAB_URI) {
 
 config.debug = true;
 config.logLevel = 7;
+retry: Infinity;
 
-var controller = Botkit.slackbot(config).configureSlackApp(
-    {
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        scopes: ['commands','bot'],
-    }
-);
+// var controller = Botkit.slackbot(config).configureSlackApp(
+//     {
+//         // clientId: process.env.CLIENT_ID,
+//         // clientSecret: process.env.CLIENT_SECRET,
+//         scopes: ['commands','bot'],
+//     }
+// );
 
-controller.setupWebserver(process.env.PORT, function (err, webserver) {
-    controller.createWebhookEndpoints(controller.webserver);
+var controller = Botkit.slackbot(config);
 
-    controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
-        if (err) {
-            res.status(500).send('ERROR: ' + err);
-        } else {
-            res.send('Success!');
-        }
-    });
-});
+var beepboop = BeepBoop.start(controller, { debug: true });
+
+// controller.setupWebserver(process.env.PORT, function (err, webserver) {
+//     controller.createWebhookEndpoints(controller.webserver);
+
+//     controller.createOauthEndpoints(controller.webserver, function (err, req, res) {
+//         if (err) {
+//             res.status(500).send('ERROR: ' + err);
+//         } else {
+//             res.send('Success!');
+//         }
+//     });
+// });
 
 
 //
 // BEGIN EDITING HERE!
 //
 
-var _bots = {};
-function trackBot(bot) {
-  _bots[bot.config.token] = bot;
-}
+// var _bots = {};
+// function trackBot(bot) {
+//   _bots[bot.config.token] = bot;
+// }
 
-controller.on('create_bot',function(bot,config) {
+// controller.on('create_bot',function(bot,config) {
 
-  if (_bots[bot.config.token]) {
-    // already online! do nothing.
-  } else {
-    bot.startRTM(function(err) {
+//   if (_bots[bot.config.token]) {
+//     // already online! do nothing.
+//     console.log("Bot is online!");
+//   } else {
+//     bot.startRTM(function(err) {
 
-      if (!err) {
-        trackBot(bot);
-      }
+//       if (!err) {
+//         trackBot(bot);
+//       }
 
-      bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
-        if (err) {
-          console.log(err);
-        } else {
-          convo.say('I am a bot that has just joined your team');
-          convo.say('You must now /invite me to a channel so that I can be of use!');
-        }
-      });
+//       bot.startPrivateConversation({user: config.createdBy},function(err,convo) {
+//         if (err) {
+//           console.log(err);
+//         } else {
+//           convo.say('I am a bot that has just joined your team');
+//           convo.say('You must now /invite me to a channel so that I can be of use!');
+//         }
+//       });
 
-    });
-  }
+//     });
+//   }
 
-});
+// });
 
-// Handle events related to the websocket connection to Slack
-controller.on('rtm_open',function(bot) {
-  console.log('** The RTM api just connected!');
-});
+// // Handle events related to the websocket connection to Slack
+// controller.on('rtm_open',function(bot) {
+//   console.log('** The RTM api just connected!');
+// });
 
-controller.on('rtm_close',function(bot) {
-  console.log('** The RTM api just closed');
-  // you may want to attempt to re-open
-});
+// controller.on('rtm_close',function(bot) {
+//   console.log('** The RTM api just closed');
+//   // you may want to attempt to re-open
+// });
 
-controller.storage.teams.all(function(err,teams) {
+// controller.storage.teams.all(function(err,teams) {
 
-  if (err) {
-    throw new Error(err);
-  }
+//   if (err) {
+//     throw new Error(err);
+//   }
 
-  console.log("Teams:", teams);
-  // connect all teams with bots up to slack!
-  for (var t  in teams) {
-    if (teams[t].bot) {
-      controller.spawn(teams[t]).startRTM(function(err, bot) {
-        if (err) {
-          console.log('Error connecting bot to Slack:',err);
-        } else {
-          trackBot(bot);
-        }
-      });
-    }
-  }
+//   console.log("Teams:", teams);
+//   // connect all teams with bots up to slack!
+//   for (var t  in teams) {
+//     if (teams[t].bot) {
+//       controller.spawn(teams[t]).startRTM(function(err, bot) {
+//         if (err) {
+//           console.log('Error connecting bot to Slack:',err);
+//         } else {
+//           trackBot(bot);
+//         }
+//       });
+//     }
+//   }
 
-});
+// });
 
 controller.on('slash_command', function (slashCommand, message) {
 
@@ -237,6 +244,74 @@ controller.on('slash_command', function (slashCommand, message) {
 
     }
 
+});
+
+beepboop.on('add_resource', function (msg) {
+  console.log('received request to add bot to team')
+});
+
+// Send the user who added the bot to their team a welcome message the first time it's connected
+beepboop.on('botkit.rtm.started', function (bot, resource, meta) {
+  var slackUserId = resource.SlackUserID
+
+  if (meta.isNew && slackUserId) {
+    bot.api.im.open({ user: slackUserId }, function (err, response) {
+      if (err) {
+        return console.log(err)
+      }
+      var dmChannel = response.channel.id
+      bot.say({channel: dmChannel, text: 'Thanks for adding me to your team!'})
+      bot.say({channel: dmChannel, text: 'Just /invite me to a channel!'})
+    })
+  }
+});
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "I'm here!")
+});
+
+controller.hears(['hello', 'hi'], ['direct_mention'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+});
+
+controller.hears(['hello', 'hi'], ['direct_message'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+  bot.reply(message, 'It\'s nice to talk to you directly.')
+});
+
+controller.hears('.*', ['mention'], function (bot, message) {
+  bot.reply(message, 'You really do care about me. :heart:')
+});
+
+controller.hears('help', ['direct_message', 'direct_mention'], function (bot, message) {
+  var help = 'I will respond to the following messages: \n' +
+      '`bot hi` for a simple message.\n' +
+      '`@<your bot\'s name>` to demonstrate detecting a mention.\n' +
+      '`bot help` to see this again.'
+  bot.reply(message, help)
+});
+
+// controller.hears(['attachment'], ['direct_message', 'direct_mention'], function (bot, message) {
+//   var text = 'Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots.'
+//   var attachments = [{
+//     fallback: text,
+//     pretext: 'We bring bots to life. :sunglasses: :thumbsup:',
+//     title: 'Host, deploy and share your bot in seconds.',
+//     image_url: 'https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png',
+//     title_link: 'https://beepboophq.com/',
+//     text: text,
+//     color: '#7CD197'
+//   }]
+
+//   bot.reply(message, {
+//     attachments: attachments
+//   }, function (err, resp) {
+//     console.log(err, resp)
+//   })
+// })
+
+controller.hears('.*', ['direct_message', 'direct_mention'], function (bot, message) {
+  bot.reply(message, 'Sorry <@' + message.user + '>, I don\'t understand. \n')
 });
 
 function getCurrentTimestamp() {
